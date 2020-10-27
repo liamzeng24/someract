0ead982e4895cd53f5b25c62435bb6a52384ddee
liamzeng24 <leamzeng@gmail.com>
2020-10-27 16:54:08 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	new file:   src/ApprovalCard.js 	new file:   src/CommentDetail.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-  return <div>Hi there! I am Liam</div>;
-};
+  return (
+    <div className="ui comments container">
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM"   
+          commentItself="Hi, I am Sam. Good to meet you!"  
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Lindsay" 
+          timeAgo="Yesterday at 1:22AM"
+          commentItself="I think Liam is god dame attractive!"
+          avatar={faker.image.avatar()}  
+        />
+      </ApprovalCard>
+    </div>
+  );
+}; 
+
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
