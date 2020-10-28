f6006624ee051427d2c3fc19856724210104ff9d
liamzeng24 <leamzeng@gmail.com>
2020-10-27 21:50:12 -0400

    deleted:    src/ApprovalCard.js 	deleted:    src/CommentDetail.js 	modified:   src/index.js 	new file:   src/seasonDisplay.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
-
-const App = () => {
-  return (
-    <div className="ui comments container">
-      <ApprovalCard>
-        <CommentDetail 
-          author="Sam" 
-          timeAgo="Today at 4:45PM"   
-          commentItself="Hi, I am Sam. Good to meet you!"  
-          avatar={faker.image.avatar()} 
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail 
-          author="Lindsay" 
-          timeAgo="Yesterday at 1:22AM"
-          commentItself="I think Liam is god dame attractive!"
-          avatar={faker.image.avatar()}  
-        />
-      </ApprovalCard>
-    </div>
-  );
-}; 
+import seasonDisplay from "./seasonDisplay";
 
 
+
+class App extends React.Component {
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>
+  }
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+ 
