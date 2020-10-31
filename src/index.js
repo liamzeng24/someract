2a6d59a4a4ca4f11de9e9460fef6992e3f395fbe
liamzeng24 <leamzeng@gmail.com>
2020-10-31 11:24:33 -0400

    modified:   src/index.js 	deleted:    src/seasonDisplay.js

--- src/index.js
+++ src/index.js
@@ -2,35 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from './seasonDisplay';
 import Loader from './loader';
+import App from './components/App';
 
-
-
-class App extends React.Component {
-  state = {lat: null, errorMessage: ''};
-
-  componentDidMount(){
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat : position.coords.latitude});
-      },
-      (err) => 
-      {
-        this.setState({errorMessage: err.message});
-      }
-    );
-  };
-
-
-  render() {
-    if(this.state.lat === null)
-      return <Loader message="Please accept request"/>
-    return <SeasonDisplay 
-            lat={this.state.lat}  
-            errorMessage={this.state.errorMessage}
-          />
-  };
-  
-};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
  
