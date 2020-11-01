d45cacdd4c7749203280421506b5e94835173e46
liamzeng24 <leamzeng@gmail.com>
2020-11-01 18:04:51 -0500

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './seasonDisplay';
-import Loader from './loader';
 import App from './components/App';
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
  
