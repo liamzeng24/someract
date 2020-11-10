c5934de666494f4b80d43dfac775c6499c1fd401
liamzeng24 <leamzeng@gmail.com>
2020-11-09 22:51:12 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term){
-        console.log(term);
+        axios.get("https://api.unsplash.com/search/photos",{
+
+        });
     }
     render(){
         return (
