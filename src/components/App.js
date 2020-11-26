287fdfd223986e5d677a5f2254c933eadf7d64de
liamzeng24 <leamzeng@gmail.com>
2020-11-26 10:50:02 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/App.js 	deleted:    src/components/SearchBar.js 	modified:   src/index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,9 @@
-import { render } from "@testing-library/react";
-import React, { useState } from "react";
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
-import Youtube from '../API/YoutubeAPI';
-import VideoDetail from './VideoDetail';
+import PostList from './postList';
 
 const App = () => {
-    const [terms, updateTerms] = useState([]);
-    const [selectedVideo, updateVideo] = useState(null);
-
-    const onSubmitSearch = async (term) =>{
-        const response = await Youtube.get('/search', {
-            params: {q: term}
-        })
-        updateTerms(response.data.items);
-        updateVideo(response.data.items[0]);
-    }
-
-    const onVideoSelect = (term) => {
-        ;
-    }
-
-    return (
-            <div className="ui container">
-                <SearchBar onTermSubmit={onSubmitSearch}/>
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={selectedVideo}/>
-                        </div>
-                        <div className="five wide column">
-                            <VideoList theList={terms} onVideoSelect={(term) => {updateVideo(term)}} />
-                        </div>
-                    </div>
-                </div>
+    return <div className='ui container'>
+                <PostList />
             </div>
-            );
 };
-
-
-
 
 export default App;
